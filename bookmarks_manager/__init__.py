9ba81dcc348660fdbd8ba5679266876af40b078c
gustavo-sopena <gsopena7@gmail.com>
2024-08-11 17:15:00 -0700

    Add static for css files and derive index.html from base.html
    - Add button to home page to submit text
    - Add check for incoming "post" requests

--- bookmarks_manager/__init__.py
+++ bookmarks_manager/__init__.py
@@ -2,7 +2,8 @@
 # date started: 2024-08-10 at 1530
 
 import os
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request
 
 def create_app(test_config=None) -> Flask:
     '''This function will create the Flask application.'''
@@ -26,10 +27,17 @@ def create_app(test_config=None) -> Flask:
     from bookmarks_manager import db
     db.register_database(app)
 
-    @app.route('/', methods=['GET'])
+    @app.route('/', methods=['GET', 'POST'])
     def index() -> str:
         '''This function is the index page of the website.'''
 
-        return render_template('base.html')
+        if request.method == 'POST':
+            url_text: str = request.form.get('user_url_input')
+            date_added: datetime = datetime.now()
+
+            print(f"date_added: {date_added}")
+            print(f"url_text: {url_text}")
+
+        return render_template('index.html')
 
     return app
